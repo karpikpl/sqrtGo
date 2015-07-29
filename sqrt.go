d06562951efb077155bc0b649c419379241592d1
Piotr Karpala <pkarpala@infusion.com>
2015-07-29 08:48:22 +0200

    fixed formatting

--- sqrt.go
+++ sqrt.go
@@ -6,21 +6,22 @@ import (
 )
 
 func Sqrt(x float64) (float64, int) {
-	z := x/2
+	z := x / 2
 	oldZ := float64(0)
 	i := 1
-	
-	for math.Abs(oldZ - z) > 0.001 {
+
+	for math.Abs(oldZ-z) > 0.001 {
 		oldZ = z
-		z = z - (z*z - x)/2*z
+		z = z - (z*z-x)/2*z
 		i++
 	}
-	
+
 	return z, i
 }
 
 func Sqrt2(x float64, z float64, i int) (float64, int) {
-	if newZ := z - (z*z - x)/2.0*z; math.Abs(newZ - z) < 0.001 {
+	i++
+	if newZ := z - (z*z-x)/2.0*z; math.Abs(newZ-z) < 0.001 {
 		return newZ, i
 	} else {
 		return Sqrt2(x, newZ, i)
@@ -28,10 +29,10 @@ func Sqrt2(x float64, z float64, i int) (float64, int) {
 }
 
 func main() {
-	
+
 	result, iterations := Sqrt(3)
 	result2, iterations2 := Sqrt2(3, 3/2.0, 0)
-	
+
 	fmt.Println("Result is", result, "after", iterations, "iterations and math.Sqrt is", math.Sqrt(3))
 	fmt.Println("Result of Sqrt(3) is", result2, "after", iterations2)
 }
